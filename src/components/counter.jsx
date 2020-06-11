49ce6382f69fc575f685a3ad7caa219219d2ef4f
iCosteaa <i.ioana3133@yahoo.com>
2020-06-11 21:41:01 +0300

    ok

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
