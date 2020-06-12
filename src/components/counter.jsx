add40df3b143a228f18c8c149b2220ce34610af5
iCosteaa <i.ioana3133@yahoo.com>
2020-06-12 23:35:14 +0300

    testing

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: [],
   };
 
   styles = {
@@ -10,21 +11,39 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  renderTags() {
+    if (this.state.tags.length == 0) return <p>idkidk</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
-        <span style={this.styles} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button> */}
+        {this.state.tags.length === 0 && "please"}
+        {this.renderTags()}
       </div>
     );
+  }
+  /* 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
     const { count } = this.state;
     return count === 0 ? <h1>Zero</h1> : count;
-  }
+  } */
 }
 
 export default Counter;
