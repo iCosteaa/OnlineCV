9776fbcd1b308d99af697ef348a7187944cb0d62
iCosteaa <i.ioana3133@yahoo.com>
2020-06-17 21:44:13 +0300

    no about

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./Header.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { Homepage } from "../Pages/Homepage/Homepage.jsx";
-import About from "../Pages/About/about.jsx";
+
 import Contact from "../Pages/Contact/contact.jsx";
 import Other from "../Pages/Other/other.jsx";
 
@@ -11,22 +11,26 @@ export class Header extends Component {
     return (
       <Router>
         <nav className="navbar">
-          <button className="btn" id="homeBtn">
-            <Link to="/">Home</Link>
-          </button>
-          <button className="btn" id="aboutBtn">
-            <Link to="/about">About</Link>
-          </button>
-          <button className="btn" id="hireBtn">
-            <Link to="/contact">HIRE ME</Link>
-          </button>
-          <button className="btn" id="otherBtn">
-            <Link to="/other">Other Projects</Link>
-          </button>
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <button className="btn" id="homeBtn">
+              Home
+            </button>
+          </Link>
+
+          <Link to="/contact">
+            <button className="btn" id="hireBtn">
+              <span>HIRE ME</span>
+            </button>
+          </Link>
+
+          <Link to="/other" className="link">
+            <button className="btn" id="otherBtn">
+              Other Projects
+            </button>
+          </Link>
         </nav>
 
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/about" component={About} />
         <Route exact path="/contact" component={Contact} />
         <Route exact path="/other" component={Other} />
       </Router>
