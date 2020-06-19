ac1779ddc4bded5ed7a97e195ec219ec92eed39f
iCosteaa <i.ioana3133@yahoo.com>
2020-06-19 20:58:41 +0300

    all good finished? about page

---
+++ src/Components/Pages/About/about.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import "./about.css";
+
+export class About extends Component {
+  render() {
+    return (
+      <div id="aboutWrapper">
+        <div id="aboutTitle">
+          <h1 id="aboutH1">About me</h1>
+        </div>
+
+        <div id="birth">
+          <div id="firstCircle">
+            <p className="dmy" id="year">
+              2000
+            </p>
+          </div>
+          <div id="secondCircle">
+            <p className="dmy" id="day">
+              3rd
+            </p>
+          </div>
+          <div id="thirdCircle">
+            <p className="dmy" id="born">
+              I was born on
+            </p>
+            <p className="dmy" id="month">
+              March
+            </p>
+          </div>
+        </div>
+
+        <div id="aboutContent">
+          <div id="aboutAndCircles">
+            <div id="aboutText">
+              <div id="aboutSummary">
+                A creative student who thinks outside the box and continously
+                seeks to learn and grow in a work environment that would give me
+                a break for my career path.
+              </div>
+            </div>
+            <div id="circles">
+              <div className="dataCircle">
+                <span className="percentageTrait">40%</span>
+                <span className="nameTrait">Creativity</span>
+              </div>
+              <div className="dataCircle" id="ambition">
+                <span className="percentageTrait" id="ambitionP">
+                  30%
+                </span>
+                <span className="nameTrait" id="ambitionT">
+                  Ambition
+                </span>
+              </div>
+              <div className="dataCircle" id="intelligence">
+                <span className="percentageTrait">30%</span>
+                <span className="nameTrait">Intelligence</span>
+              </div>
+              <span id="eq">=</span>
+              <div className="dataCircle" id="total">
+                <span className="percentageTrait">100%</span>
+                <span className="nameTrait">Willingness to learn!</span>
+              </div>
+            </div>
+          </div>
+          <div id="places">
+            <div id="petrosani"></div>
+            <div id="placeData">
+              <p>
+                I am from <span>Petroșani</span> but I currently live in{" "}
+                <span>Cluj</span>
+              </p>
+            </div>
+            <div id="cluj"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
--- src/Components/Pages/Homepage/Homepage.jsx
+++ src/Components/Pages/Homepage/Homepage.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import "./Homepage.css";
-import img from "./me.png";
-import petrosani from "./petrosani.png";
-import cluj from "./cluj.png";
+import { About } from "../About/about.jsx";
 
 export class Homepage extends Component {
   render() {
@@ -13,38 +11,11 @@ export class Homepage extends Component {
             <span id="nameSpan">COSTEA IOANA</span>
           </h1>
           <span id="desc">
-            {" "}
-            <img src={img} id="imgMe" alt="me"></img>An unemployed and hungry
-            student looking for a job in Web Development
+            An unemployed and hungry student looking for a position in Web
+            Development
           </span>
         </div>
-
-        <div id="aboutWrapper">
-          <div id="aboutTitle"></div>
-          <h1 id="aboutH1">About me</h1>
-          <div id="aboutContent">
-            <div id="birth">
-              <div id="firstCircle"></div>
-              <div id="secondCircle"></div>
-              <div id="thirdCircle"></div>
-            </div>
-
-            <div id="aboutText">
-              <div id="places">
-                <img src="http://www.rleonardi.com/interactive-resume/image/building-a.png"></img>
-                <div id="fromLive">
-                  <p>
-                    I am from Petroșani, Hunedoara, but I currently live in
-                    Cluj-Napoca
-                  </p>
-                  <p>I am studying at "Babeș-Bolyai" University</p>
-                </div>
-                <img src="http://www.rleonardi.com/interactive-resume/image/building-a.png"></img>
-              </div>
-            </div>
-            <div></div>
-          </div>
-        </div>
+        <About />
       </div>
     );
   }
