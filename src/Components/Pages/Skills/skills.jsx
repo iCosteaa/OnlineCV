5c533c1a4e936ce2736e84cb4dff6cfa541e5ee1
iCosteaa <i.ioana3133@yahoo.com>
2020-06-25 22:14:29 +0300

    sa mor eu cu tabelul asta

--- src/Components/Pages/Skills/skills.jsx
+++ src/Components/Pages/Skills/skills.jsx
@@ -7,7 +7,7 @@ export class Skills extends Component {
       <div className="skillsWrapper">
         <h1 className="titleH1">Skills</h1>
         <div className="contentWrapper">
-          <div className="Skills">
+          <div className="firstSkills">
             <h4>Programming and 3D Modelling Skills</h4>
             <h4>HTML</h4>
             <div className="line line1">90%</div>
@@ -26,25 +26,68 @@ export class Skills extends Component {
             <h4>3Ds Max</h4>
             <div className="line line8">15%</div>
           </div>
-          <div className="Skills" id="languages">
-            <div className="speechBubble" id="english">
-              <span className="language">English</span>
-              <span>Fluent</span>
+
+          <div className="secondBox">
+            <div className="Skills" id="languages">
+              <div className="speechBubble" id="english">
+                <span className="language">English</span>
+                <span>Fluent</span>
+              </div>
+              <div id="centre">
+                <span>Languages</span>
+              </div>
+              <div className="speechBubble" id="romanian">
+                <span className="language">Romanian</span>
+                <span>Native</span>
+              </div>
+              <div className="speechBubble" id="norwegian">
+                <span className="language">Norwegian</span>
+                <span>Advanced</span>
+              </div>
+              <div className="speechBubble" id="french">
+                <span className="language">French</span>
+                <span>Beginner</span>
+              </div>
             </div>
-            <div id="centre">
-              <span>Languages</span>
-            </div>
-            <div className="speechBubble" id="romanian">
-              <span className="language">Romanian</span>
-              <span>Native</span>
-            </div>
-            <div className="speechBubble" id="norwegian">
-              <span className="language">Norwegian</span>
-              <span>Advanced</span>
-            </div>
-            <div className="speechBubble" id="french">
-              <span className="language">French</span>
-              <span>Beginner</span>
+
+            <div className="Skills" id="tableSkills">
+              <table>
+                <tbody>
+                  <tr>
+                    <td>
+                      <span>Listening</span>
+                    </td>
+                    <td>
+                      <span>Problem Solving</span>
+                    </td>
+                    <td>
+                      <span>Responsability</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span>Clarity and Concision</span>
+                    </td>
+                    <td>
+                      <span>Adaptability</span>
+                    </td>
+                    <td>
+                      <span>Commitment</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <span>Teamwork</span>
+                    </td>
+                    <td>
+                      <span>Handling Criticism</span>
+                    </td>
+                    <td>
+                      <span>Positivity</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
